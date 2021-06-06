63a37d513ab980aa4c9a71c28c5f925d8e147f53
net-ronz <8134898+net-ronz@user.noreply.gitee.com>
2021-06-06 15:42:42 +0800

    v4.0.0.2105

--- dist/wwwroot/storage/data/app-data.js
+++
@@ -1,8 +0,0 @@
-var myId = {@myid};
-var myUsername = '{@myusername}';
-var myAdmin = {@myadmin};
-var myUploadSize = {@myuploadsize};
-var myDownloadSize = {@mydownloadsize};
-var uploadExtension = '{@uploadextension}';
-var retentionDays = {@retentiondays};
-var systemDate = new Date('{@systemdate}');
